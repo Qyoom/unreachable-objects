548069ca695c12acaf2b0ac35e1e3f611cddb33e
Qyoom <aqumegeemail@gmail.com>
2023-08-30 14:50:36 -0700

    gsap-timeline-squares_6 commit
    Dynamic svg size to conform with other carousel visualizations on horizontal screens.

---
+++ posts/rotate_squares_gsap/gsap-timeline-squares_6.js
@@ -0,0 +1,120 @@
+console.log('gsap-timeline-squares_6.js');
+
+function GsapSquaresDrawing() { // module, closure
+  console.log("In GsapSquaresDrawing TOP");
+
+  const windowSize = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  };
+
+  const svgSize = windowSize.width < windowSize.height ? 
+    windowSize.width * 0.90 : 
+    windowSize.height * 0.82;
+    
+  const qtySquares = svgSize > 500 ? 25 : 20;
+  const stepSize = (svgSize / qtySquares) / 2;
+
+  let playingFlag = false;
+  let playCompleteFlag = false;
+
+  // https://stackoverflow.com/questions/1484506/random-color-generator
+  function randomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  let squareData = [];
+
+  function makeSquareData() {
+    console.log("makeSquareData() called");
+    squareData = [];
+    for (let i = 0; i < qtySquares; i++) {
+      squareData.push({
+        squareSize: svgSize - (i * stepSize * 2),
+        color: randomColor(),
+        x: i * stepSize,
+        y: i * stepSize
+      })
+    }
+    // console.log(`squareData.length: ${squareData.length}`);
+  }
+
+  const svg = d3.select("#gsap-squares")
+    .append("svg")
+      .attr("width", svgSize)
+      .attr("height", svgSize);
+
+  function makeSquares() {
+    console.log("makeSquares() called");
+    const squares = svg.selectAll('.square')
+      .data(squareData)
+      .join("rect")
+        .attr("x", (d) => { return d.x; })
+        .attr("y", (d) => { return d.y; })
+        .attr("width", (d) => { return d.squareSize; })
+        .attr("height", (d) => { return d.squareSize; })
+        .attr("fill", (d) => { return d.color })
+        .attr("class", 'square');
+    // console.log('makeSquares squares:', squares);
+  }
+
+  let tl = null;
+
+  function play() {
+    console.log("play() called");
+    makeSquareData();
+    svg.selectAll('.square').remove(); // Not sure of my footing with D3 yet!
+    makeSquares(); // start with whole new set of rects
+    tl = null; // wipe out vestages of previous rects
+    tl = gsap.timeline({ 
+      onStart: () => console.log("the tween started"),
+      onComplete: () => {
+        console.log("the tween is complete");
+        playingFlag = false;
+        playCompleteFlag = true;
+      },
+      onReverseComplete: () => console.log("the reverse tween is complete")
+    });
+    tl.from('.square', {
+      rotation: 360,
+      transformOrigin: "50%, 50%",
+      scale: 0.001,
+      duration: 2.5,
+      ease: "power1.inOut",
+      stagger: 1
+    });
+    playingFlag = true; 
+    playCompleteFlag = false;
+  } // end play
+
+  function reverse() { 
+    console.log("reverse() called");
+    tl.reverse();
+    playingFlag = false;
+    playCompleteFlag = false;
+  }
+
+  // Toggle play/reverse logic
+  function toggleGsapSquares() {
+    console.log("In toggleGsapSquares TOP");
+    console.log(`playingFlag: ${playingFlag}, playCompleteFlag: ${playCompleteFlag}`);
+
+    if(playingFlag == false && playCompleteFlag == false) { 
+      play();
+    } else if(playingFlag == true || playCompleteFlag == true){
+      reverse();
+    };
+  }
+
+  return {
+    toggleGsapSquares: toggleGsapSquares
+  };
+} // end GsapSquaresDrawing
+
+
+
